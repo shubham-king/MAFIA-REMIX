2b1519d0dfdedc05edcde9e8b3bcaaf5109b021d
Satyam <128127471+Raichu70@users.noreply.github.com>
2023-04-20 23:28:05 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -44,12 +44,12 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                 text="❣ sᴜᴩᴩᴏʀᴛ ❣", url=config.SUPPORT_GROUP
             ),
             InlineKeyboardButton(
-                text="🥀 ᴍᴀɪɴᴛᴀɪɴᴇʀ 🥀", user_id=OWNER
+                text="😎 ᴍᴀɪɴᴛᴀɪɴᴇʀ 😎", user_id=OWNER
             )
         ],
         [
             InlineKeyboardButton(
-                text="✨ sᴏᴜʀᴄᴇ ✨", url=config.UPSTREAM_REPO
+                text="💫 sᴏᴜʀᴄᴇ 💫", url=config.UPSTREAM_REPO
             )
         ],
      ]
